8fa7f046de47c69223ac5c50a031d8ab0e5c40d1
bruno cuconato (@odanoburu) <bcclaro@gmail.com>
2024-05-16 08:22:36 -0300

    Python Indentation

--- webapp.py
+++ webapp.py
@@ -17,29 +17,29 @@ app.secret_key = os.getenv("FLASK_SECRET_KEY")
 DATABASE = None
 
 def db_query_db(db, query, args=(), one=False):
-    cur = db.execute(query, args)
-    rv = cur.fetchall()
-    cur.close()
-    return (rv[0] if rv else None) if one else rv
+  cur = db.execute(query, args)
+  rv = cur.fetchall()
+  cur.close()
+  return (rv[0] if rv else None) if one else rv
 
 def get_db():
-    db = getattr(g, '_database', None)
-    if db is None:
-        db = sqlite3.connect(DATABASE)
-        db.row_factory = sqlite3.Row
-        g._database = db
-        db_query_db(db, "PRAGMA foreign_keys=ON")
-    return db
+  db = getattr(g, '_database', None)
+  if db is None:
+    db = sqlite3.connect(DATABASE)
+    db.row_factory = sqlite3.Row
+    g._database = db
+    db_query_db(db, "PRAGMA foreign_keys=ON")
+  return db
 
 def query_db(query, args=(), one=False):
-    db = get_db()
-    return db_query_db(db, query, args, one)
+  db = get_db()
+  return db_query_db(db, query, args, one)
 
 @app.teardown_appcontext
 def close_connection(exception):
-    db = getattr(g, '_database', None)
-    if db is not None:
-        db.close()
+  db = getattr(g, '_database', None)
+  if db is not None:
+    db.close()
 
 
 #### Authentication
@@ -47,42 +47,42 @@ def close_connection(exception):
 USER_SESSION_KEY = 'USERNAME'
 
 def login_and_redirect(username, next_url=''):
-    ## CAUTION: only call this function after verifying user
-    ## authentication
+  ## CAUTION: only call this function after verifying user
+  ## authentication
 
-    # NOTE: authentication is done simply by creating this key in
-    # the session object, which translates to signed cookie in the
-    # browser. Because the cookie is signed with our private secret
-    # key, no user can fake being another one by editing the cookie
-    # (which would render it invalid)
-    session[USER_SESSION_KEY] = username
-    if not next_url:
-      next_url = url_for('view_app')
-    return redirect(next_url)
+  # NOTE: authentication is done simply by creating this key in
+  # the session object, which translates to signed cookie in the
+  # browser. Because the cookie is signed with our private secret
+  # key, no user can fake being another one by editing the cookie
+  # (which would render it invalid)
+  session[USER_SESSION_KEY] = username
+  if not next_url:
+    next_url = url_for('view_app')
+  return redirect(next_url)
 
 def get_user_id(username):
-    res = query_db("""SELECT id AS user_id
+  res = query_db("""SELECT id AS user_id
   FROM users
   WHERE username = ?""", (username,), one=True)
-    if res is not None:
-      user_id = res['user_id']
-      g._user_id = user_id
-      return user_id
+  if res is not None:
+    user_id = res['user_id']
+    g._user_id = user_id
+    return user_id
 
 def login_required(f):
-    @wraps(f)
-    def decorated_function(*args, **kwargs):
-        if getattr(g, '_user_id', None) is None:
-            username = session.get(USER_SESSION_KEY)
-            if username is None:
-              return redirect(url_for('view_login', next=request.url))
-            else:
-              user_id = get_user_id(username)
-              if user_id is None:
-                return redirect(url_for('logout'))
-              g._username = username
-        return f(*args, **kwargs)
-    return decorated_function
+  @wraps(f)
+  def decorated_function(*args, **kwargs):
+    if getattr(g, '_user_id', None) is None:
+      username = session.get(USER_SESSION_KEY)
+      if username is None:
+        return redirect(url_for('view_login', next=request.url))
+      else:
+        user_id = get_user_id(username)
+        if user_id is None:
+          return redirect(url_for('logout'))
+        g._username = username
+    return f(*args, **kwargs)
+  return decorated_function
 
 ##### Authentication Routes
 
@@ -160,48 +160,48 @@ def logout():
 
 @app.route("/", methods=["GET"])
 def index():
-    return render_template("index.html")
+  return render_template("index.html")
 
 @app.route('/app', methods=["GET"])
 @login_required
 def view_app():
-    return render_template("app.html", username=g._username)
+  return render_template("app.html", username=g._username)
 
 @app.route('/messaged', methods=["POST"])
 @login_required
 def message_new():
-    # add message to session
-    # db = get_db()
-    #   cur = db.execute("""INSERT INTO groups(name, description)
-    # VALUES (:name, :description)
-    # RETURNING id AS group_id""",
-    #           {'name': name, 'description': description})
-    #   res = cur.fetchone()
-    #   if res is not None:
-    #     group_id = res['group_id']
-    #db.commit()
-    user_message = request.form.get('user_message', None)
-    if user_message is None:
-        return "" # no HTML response
-    # get answer from AI
-    # # TODO: get answer from AI https://platform.openai.com/docs/api-reference/chat/create
-    # # TODO: break it into paragraphs <p>
+  # add message to session
+  # db = get_db()
+  #   cur = db.execute("""INSERT INTO groups(name, description)
+  # VALUES (:name, :description)
+  # RETURNING id AS group_id""",
+  #           {'name': name, 'description': description})
+  #   res = cur.fetchone()
+  #   if res is not None:
+  #     group_id = res['group_id']
+  #db.commit()
+  user_message = request.form.get('user_message', None)
+  if user_message is None:
+    return "" # no HTML response
+  # get answer from AI
+  # # TODO: get answer from AI https://platform.openai.com/docs/api-reference/chat/create
+  # # TODO: break it into paragraphs <p>
 
-    # TODO: store client in session data
-    # client = OpenAI()
-    # response = client.chat.completions.create(
-    #     model="gpt-3.5-turbo",
-    #     messages=[
-    #         # TODO: include ontology in system message?
-    #         {"role": "system", "content": "You are a helpful assistant."},
-    #         {"role": "user", "content": user_message}
-    #     ]
-    # )
-    # # TODO: check if response was ok
-    # assistant_message = response.choices[0].message
-    assistant_message = 'The answer to life, the universe, and everything is… 42.'
-    # TODO: include user message client-side
-    return render_template("message.html", username=g._username, user_message=user_message, ainame="AI", assistant_message=assistant_message)
+  # TODO: store client in session data
+  # client = OpenAI()
+  # response = client.chat.completions.create(
+  #     model="gpt-3.5-turbo",
+  #     messages=[
+  #         # TODO: include ontology in system message?
+  #         {"role": "system", "content": "You are a helpful assistant."},
+  #         {"role": "user", "content": user_message}
+  #     ]
+  # )
+  # # TODO: check if response was ok
+  # assistant_message = response.choices[0].message
+  assistant_message = 'The answer to life, the universe, and everything is… 42.'
+  # TODO: include user message client-side
+  return render_template("message.html", username=g._username, user_message=user_message, ainame="AI", assistant_message=assistant_message)
 
 class NotAuthorized(Exception):
   "Raised when the user does not have the authority to perform some action"
@@ -209,24 +209,24 @@ class NotAuthorized(Exception):
 
 @app.errorhandler(NotAuthorized)
 def handle_bad_request(e):
-    return render_template("error.html", what="Unauthorized"), 401
+  return render_template("error.html", what="Unauthorized"), 401
 
 @app.errorhandler(404)
 def page_not_found(e):
-    # note that we set the 404 status explicitly
-    return render_template('error.html', what="Page not found"), 404
+  # note that we set the 404 status explicitly
+  return render_template('error.html', what="Page not found"), 404
 
 def start_webapp(host=None, port=None, debug_mode=None, db_path=None):
-    global DATABASE
-    if host is None:
-        host = 'localhost'
-    if port is None:
-        port = 8888
-    if debug_mode is None:
-      # INFO: this is to debug the server; to debug openai library
-      # we can set the environment variable OPENAI_LOG=debug
-      debug_mode = True
-    if db_path is None:
-        db_path = 'assistonto.db'
-    DATABASE = db_path
-    app.run(host=host, port=port, debug=debug_mode)
+  global DATABASE
+  if host is None:
+    host = 'localhost'
+  if port is None:
+    port = 8888
+  if debug_mode is None:
+    # INFO: this is to debug the server; to debug openai library
+    # we can set the environment variable OPENAI_LOG=debug
+    debug_mode = True
+  if db_path is None:
+    db_path = 'assistonto.db'
+  DATABASE = db_path
+  app.run(host=host, port=port, debug=debug_mode)
